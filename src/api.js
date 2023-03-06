7e473ae86c15ffd6108d0ebb9e7951143a0b6028
Annany Vishwakarma <kingvansh009@gmail.com>
2023-03-06 19:04:46 +0530

    build folder added

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9c55d21519msh47a740bc13cdf63p18551cjsn98a893f1bf86',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "37e7391ce88ae7c593adb9ea144b05de";
